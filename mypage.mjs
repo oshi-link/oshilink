5e456d07b32dcf6825fab8ff5fd590c936cb025c
oshi-link <sdaik29dsuzuk@gmail.com>
2026-09-06 16:08:24 +0900

    Refresh direct-drag image editor modules

--- mypage.mjs
+++ mypage.mjs
@@ -1,7 +1,7 @@
 import { voiceTags, tagDisabled, validFlyer, allowedPanels, lpIntro } from './mypage-rules.mjs?v=required-3';
 import {bindImagePreview} from './local-image-preview.mjs?v=drag-1';
 import {buildProfilePayload,saveMyProfiles,loadMyProfiles,profileFormValues} from './profile-save.mjs?v=prefill-1';
-import {saveSelectedProfileImages,showSavedProfileImages} from './profile-images.mjs?v=crop-1';
+import {saveSelectedProfileImages,showSavedProfileImages} from './profile-images.mjs?v=drag-1';
 import {buildVideoPost,saveMyVideo,buildLivePost,findEventCandidates,saveMyEvent} from './posting-save.mjs';
 import {loadMyContent,setContentPublic,publishMyEvent} from './content-management.mjs';
 import {supabase} from './supabase-client.mjs';
